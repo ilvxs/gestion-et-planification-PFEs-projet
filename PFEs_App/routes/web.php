08abb9a1695e5b1543b5f23c3a948c50fc498816
nassim-not1 <nassimelgourari7@gmail.com>
2026-05-13 00:51:56 +0100

    modification des routes

--- PFEs_App/routes/web.php
+++ PFEs_App/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImportController;
 
-Route::get('/imports', [ImportController::class, 'index'])
+Route::get('/', [ImportController::class, 'index'])
     ->name('imports.index');
 
-Route::post('/imports', [ImportController::class, 'importAll'])
+Route::post('/', [ImportController::class, 'importAll'])
     ->name('imports.all');
 
